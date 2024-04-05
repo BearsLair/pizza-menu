a6e2ade3771bfb9ed15e7453e0dd53023255202e
Patrick Gross <bears_lair@protonmail.com>
2024-04-04 23:58:56 -0700

    Basic site layout in progress

--- pizza menu/src/App.jsx
+++ pizza menu/src/App.jsx
@@ -1,13 +1,24 @@
 import Pizza from "./components/Pizza";
 import "./App.css";
+import pizzaList from "./pizza-data";
+
+console.log(pizzaList);
 
 function App() {
   return (
     <>
       <h1 className="title">Little Bear Pizza Co.</h1>
       <hr />
-      <div>
-        <Pizza />
+      <div className="pizza-list">
+        {pizzaList.map((pizza) => (
+          <Pizza
+            name={pizza.name}
+            img={pizza.img}
+            description={pizza.description}
+            id={pizza.id}
+            key={pizza.id}
+          />
+        ))}
       </div>
     </>
   );
