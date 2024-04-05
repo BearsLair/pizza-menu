57e928cd59411a230b385dce7de2a43c3bf9d8eb
Patrick Gross <bears_lair@protonmail.com>
2024-04-04 23:02:23 -0700

    initial commit

---
+++ pizza menu/src/App.jsx
@@ -0,0 +1,16 @@
+import Pizza from "./components/Pizza";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="title">Little Bear Pizza Co.</h1>
+      <hr />
+      <div>
+        <Pizza />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ pizza menu/src/components/Pizza.jsx
@@ -0,0 +1,14 @@
+function Pizza() {
+  return (
+    <div className="pizza-card">
+      <p className="pizza-title">Pepperoni Pizza</p>
+      <img
+        className="pizza-img"
+        src="src\assets\pexels-polina-tankilevitch-4109073.jpg"
+      />
+      <button className="pizza-button">Add to cart</button>
+    </div>
+  );
+}
+
+export default Pizza;
