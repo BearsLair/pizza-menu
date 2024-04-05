b339f00710c96579a1cbdeb2b03788fe0375bdaa
Patrick Gross <bears_lair@protonmail.com>
2024-04-04 23:57:44 -0700

    created data for testing purposes

---
+++ pizza menu/src/pizza-data.js
@@ -0,0 +1,40 @@
+const pizzaList = [
+  {
+    id: 1,
+    name: "Pepperoni Pizza",
+    description: "A delicious peperoni and cheese pizza, with a thick crust.",
+    img: "src/assets/pexels-polina-tankilevitch-4109073.jpg",
+  },
+  {
+    id: 2,
+    name: "Pepperoni Pizza 2",
+    description: "A delicious peperoni and cheese pizza, with a thick crust.",
+    img: "src/assets/pexels-polina-tankilevitch-4109073.jpg",
+  },
+  {
+    id: 3,
+    name: "Pepperoni Pizza 3",
+    description: "A delicious peperoni and cheese pizza, with a thick crust.",
+    img: "src/assets/pexels-polina-tankilevitch-4109073.jpg",
+  },
+  {
+    id: 4,
+    name: "Pepperoni Pizza 4",
+    description: "A delicious peperoni and cheese pizza, with a thick crust.",
+    img: "src/assets/pexels-polina-tankilevitch-4109073.jpg",
+  },
+  {
+    id: 5,
+    name: "Pepperoni Pizza 5",
+    description: "A delicious peperoni and cheese pizza, with a thick crust.",
+    img: "src/assets/pexels-polina-tankilevitch-4109073.jpg",
+  },
+  {
+    id: 6,
+    name: "Pepperoni Pizza 6",
+    description: "A delicious peperoni and cheese pizza, with a thick crust.",
+    img: "src/assets/pexels-polina-tankilevitch-4109073.jpg",
+  },
+];
+
+export default pizzaList;
